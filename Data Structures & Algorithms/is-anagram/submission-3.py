1cb6efa96c2922e047b1a63be00e61f2e8eec115
raoanirudhm-star <raoanirudhm@gmail.com>
2026-06-16 15:17:03 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        track_letters = [0]*26
+
+        for i in range(len(s)):
+            track_letters[ord(s[i]) - ord('a')] += 1
+        
+        for i in range(len(t)):
+            ct = track_letters[ord(t[i]) - ord('a')]
+            if ct < 1:
+                return False
+            else:
+               track_letters[ord(t[i]) - ord('a')] -= 1 
+        
+        return track_letters == ([0] * 26)
+
+
+        
